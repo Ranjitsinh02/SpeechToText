8b1ec01d4365d3bb685b3fee2589864a4d14c8db
Ranjit Gohil <granjitsinh89@gmail.com>
2024-08-14 12:39:27 +0530

    - Initial Commit.

---
+++ bin/speech_to_text.dart
@@ -0,0 +1,5 @@
+import 'package:speech_to_text/speech_to_text.dart' as speech_to_text;
+
+void main(List<String> arguments) {
+  print('Hello world: ${speech_to_text.calculate()}!');
+}
---
+++ test/speech_to_text_test.dart
@@ -0,0 +1,8 @@
+import 'package:speech_to_text/speech_to_text.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
